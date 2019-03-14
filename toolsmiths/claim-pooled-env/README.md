062e30bb78f2c1a6f3c5f963b6ee16b853d9f316
Joe Gee <jgee@pivotal.io>
2019-03-14 16:12:56 -0600

    New task for claiming toolsmiths pooled environments
    Signed-off-by: Peter Stone <pstone@pivotal.io>

---
+++ toolsmiths/claim-pooled-env/README.md
@@ -0,0 +1,47 @@
+# claim-pooled-env
+
+This task uses the `toolsmiths-envs-resource` to display the metadata for a pooled environment.
+
+To use, you'll need to follow the instructions [here](toolsmiths-onboarding) to get your API token and Concourse workers set up.
+
+The accompanying `target-claimed-env.sh` script uses the output of this task to target the BOSH Director and CF API for local debugging. See [the FAQ](toolsmiths-faq) for some helpful tips.
+
+The `pcf-pool` resource type does most of the heavy lifting, so this task just reports the environment details.
+
+## Example
+
+### Configuration
+
+```yaml
+resources:
+- name: 2.3-env
+  type: pcf-pool
+  source:
+    api_token: ((toolsmiths-api-key))
+    hostname: environments.toolsmiths.cf-app.com
+    pool_name: us_2_3
+  tags: [ ((toolsmiths-workers-tag)) ]
+
+resource_types:
+- name: pcf-pool
+  type: docker-image
+  source:
+    repository: cftoolsmiths/toolsmiths-envs-resource
+
+jobs:
+- name: claim-2.3-env
+  plan:
+  - get: before-workday
+    trigger: true
+  - put: 2.3-env
+    params:
+      action: claim
+    tags: [ ((toolsmiths-workers-tag)) ]
+  - task: output-env-details
+    file: concourse-tasks/toolsmiths/claim-pooled-env/task.yml
+    input_mapping:
+      pooled-env: 2.3-env
+```
+
+[toolsmiths-onboarding]:  https://docs.google.com/document/d/1afCL7hgFeQ61orx6Z5bP49xauE753n5eSZPuO5bWJeY/edit#heading=h.rzx8m9ypluky
+[toolsmiths-faq]:         https://environments.toolsmiths.cf-app.com/faq
---
+++ toolsmiths/claim-pooled-env/target-claimed-env.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+# source this file to target your interactive shell
+
+# Optional env overrides:
+# CONCOURSE_TEAM if other than 'denver'
+# OM_PATH to save somewhere other than /tmp
+
+install-om() {
+  if ! which om >/dev/null; then
+      OM_PATH=${OM_PATH:-$(mktemp -d)}
+      curl -sL https://github.com/pivotal-cf/om/releases/latest/download/om-$(uname | tr '[:upper:]' '[:lower:]') >$OM_PATH/om
+      chmod +x $OM_PATH/om
+      export PATH="$PATH:$OM_PATH"
+  fi
+}
+
+target-om() {
+  ENV_METADATA=$1
+  export OM_USERNAME=$(jq .ops_manager.username -r ${ENV_METADATA})
+  export OM_PASSWORD=$(jq .ops_manager.password -r ${ENV_METADATA})
+  export OM_TARGET=$(jq .ops_manager.url -r ${ENV_METADATA})
+  export OM_SKIP_SSL_VALIDATION=true # TODO: we have the certs in ENV_METADATA, can we provide them to om?
+}
+
+bosh-login() {
+  ENV_METADATA=$1
+  local ops_manager_dns=$(jq .ops_manager_dns -r ${ENV_METADATA})
+  local env_name=$(jq .name -r ${ENV_METADATA})
+
+  install-om
+  target-om ${ENV_METADATA}
+
+  eval "$(om bosh-env)"
+
+  jq .ops_manager_private_key -r ${ENV_METADATA} > /tmp/${env_name}.key
+  chmod 600 /tmp/${env_name}.key
+
+  export BOSH_ALL_PROXY="ssh+socks5://ubuntu@${ops_manager_dns}:22?private-key=/tmp/${env_name}.key"
+  export CREDHUB_PROXY="ssh+socks5://ubuntu@${ops_manager_dns}:22?private-key=/tmp/${env_name}.key"
+  export SYS_DOMAIN=$(jq .sys_domain -r ${ENV_METADATA})
+
+  credhub login
+}
+
+get-cf-deployment-name() {
+  bosh deployments --json | jq -r '.Tables[].Rows | map(select(.release_s | contains("garden-runc"))) | .[0].name'
+}
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <concourse-job>"
+    return
+fi
+
+fly -t ${CONCOURSE_TEAM:-denver} w -j ${1} | tail -1  | sed 's/succeeded$//' >/tmp/claimed-metadata
+export ENV_NAME=$(jq .name -r /tmp/claimed-metadata)
+echo "Targeting ${ENV_NAME}."
+
+export ENV_METADATA="/tmp/${ENV_NAME}-metadata"
+mv /tmp/claimed-metadata ${ENV_METADATA}
+
+bosh-login ${ENV_METADATA}
+
+export CF_ADMIN_PASSWORD=$(om credentials -p cf -c .uaa.admin_credentials -f password)
+export CF_ADMIN_USER=$(om credentials -p cf -c .uaa.admin_credentials -f identity)
+cf login -a https://api.$(jq .sys_domain -r ${ENV_METADATA}) -u ${CF_ADMIN_USER} -p ${CF_ADMIN_PASSWORD} --skip-ssl-validation
+
+jq .ops_manager.password -r ${ENV_METADATA} | pbcopy
+echo "OpsMan password copied, log in at: $(jq .ops_manager.url -r ${ENV_METADATA})"
