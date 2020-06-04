018663068fab25e0c76b6f72e08f322886dd6d04
Caitlyn Yu <cyu@pivotal.io>
2020-06-04 20:42:16 +0000

    Add ability to use cfd-env in export task
    Signed-off-by: Kira Boyle <kboyle@pivotal.io>

--- helpers/environment-targeting.sh
+++ helpers/environment-targeting.sh
@@ -4,6 +4,8 @@ function target-bosh() {
   if [[ -d pas-env ]]; then
     source $(dirname $BASH_SOURCE)/toolsmiths.sh
     bosh-login $PWD/pas-env/metadata
+  elif [[ -d cfd-env ]]; then
+      eval "$(bbl print-env --metadata-file cfd-env/metadata)"
   elif [[ -d bbl-state ]]; then
     pushd "bbl-state/$BBL_STATE_DIR" > /dev/null
       eval "$(bbl print-env)"
